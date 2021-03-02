8232e3572faf159b265e9f3ebc88dd9db90fda50
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-03-02 18:24:47 +0300

    Created:sculp script

---
+++ Assets/Scripts/DeformIt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformIt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        RaycastHit hit;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.name=="Rock")
+                {
+                    Destroy(hit.collider.gameObject);
+
+                }
+            }
+        }
+        
+        
+
+    }
+}
